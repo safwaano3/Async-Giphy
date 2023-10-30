90383a80832b5d754599c725bce907a051072f2c
Abdullah Safwan <abdullah.safwan@vodafone.co.uk>
2023-10-30 16:25:15 +0000

    index.js Completed

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
+const input = document.querySelector("input");
+const container = document.querySelector(".gifs");
+
+const getImages = async (query) => {
+  const apiKey = "t8ur12HnhbN9XDWKVdOx2Q7c03vmCFyJ";
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data;
+};
+
+const handleSubmit = async (event) => {
+  if (event.target.value.trim() === "") return;
+  const gifs = await getImages(event.target.value);
+  if (!gifs) {
+    container.innerHTML = `
+        <div class="loading-container">
+        <p class="loading">Loading...</p>
+        <div>
+    `;
+  } else {
+    container.innerHTML = gifs.data
+      .map((gif) => {
+        return `<img class="gif" src=${gif.images.original.url} />`;
+      })
+      .join("");
+  }
+};
+input.addEventListener("change", (event) => handleSubmit(event));
+
+
 console.log(process.env.API_KEY)
